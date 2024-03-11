9dea0f351314991269b1566295be5d5ab5ded9fb
Eudis Rene Duarte Sanguino <eudis.duarte@rappi.com>
2024-03-11 16:49:20 -0500

    version 5

--- src/main/kotlin/com/rappi/pay/pse/domain/service/PSEPaymentQueryServiceImpl.kt
+++ src/main/kotlin/com/rappi/pay/pse/domain/service/PSEPaymentQueryServiceImpl.kt
@@ -1,7 +1,5 @@
 package com.rappi.pay.pse.domain.service
 
 import com.rappi.pay.pse.domain.port.input.PSEPaymentQueryService
-import org.springframework.stereotype.Service
 
-@Service
 class PSEPaymentQueryServiceImpl : PSEPaymentQueryService
---
+++ src/main/kotlin/com/rappi/pay/pse/infrastructure/common/config/ServiceBeanConfig.kt
@@ -0,0 +1,32 @@
+package com.rappi.pay.pse.infrastructure.common.config
+
+import com.rappi.pay.pse.domain.port.output.AccountPort
+import com.rappi.pay.pse.domain.port.output.CachePort
+import com.rappi.pay.pse.domain.port.output.NotificationPort
+import com.rappi.pay.pse.domain.port.output.PSEPaymentPort
+import com.rappi.pay.pse.domain.port.output.UserPort
+import com.rappi.pay.pse.domain.service.PSEPaymentQueryServiceImpl
+import com.rappi.pay.pse.domain.service.RegisterPSEPaymentServiceImpl
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ServiceBeanConfig {
+    @Bean
+    fun buildPSEPaymentQueryService() = PSEPaymentQueryServiceImpl()
+
+    @Bean
+    fun buildRegisterPSEPaymentService(
+        psePaymentPort: PSEPaymentPort,
+        accountPort: AccountPort,
+        userPort: UserPort,
+        notificationPort: NotificationPort,
+        cachePort: CachePort,
+    ) = RegisterPSEPaymentServiceImpl(
+        psePaymentPort,
+        accountPort,
+        userPort,
+        notificationPort,
+        cachePort,
+    )
+}
